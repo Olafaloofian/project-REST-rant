480538c74dbec29acd50df366ff5791f8f9814b0
Michael Kerr <bulluffalo@gmail.com>
2022-03-23 20:31:48 -0700

    Added bootstrap and custom CSS

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// SETTINGS
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// MIDDLEWARES
 app.use('/places', require('./controllers/places'));
 
 //Routes
